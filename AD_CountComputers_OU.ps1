637bbbef90bcd1bb3ed809a1786d41d8781d31cb
StevenADietz <46472239+StevenADietz@users.noreply.github.com>
2019-01-07 23:46:52 -0500

    Add files via upload

---
+++ AD_CountComputers_OU.ps1
@@ -0,0 +1,10 @@
+﻿Import-module activedirectory
+$OU = "OU=Computers,DC=domain,DC=local"
+Get-ADOrganizationalUnit -filter * -SearchBase $ou | % {
+$Count = (get-adcomputer -filter * -searchbase $_.distinguishedname -SearchScope 1 | Measure).count
+     If ($Count -ge 1){
+     New-Object PSobject -Property @{
+          OUDN = $_.DistinguishedName
+          Count = $Count
+           }
+     }
